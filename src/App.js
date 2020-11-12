a28a905c9822004d0db71053ee75cdfa655e7804
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-11-11 23:22:48 -0500

    search function, image links

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 
 import './App.css';
 import { Component } from "react";
-import { render } from '@testing-library/react';
-import PhotoContainer from "./PhotoContainer";
+import SearchPhotos from "./SearchPhotos";
+
 
 class App extends Component
 {
-  constructor()
-  {
-    super();
-    this.state = 
-    { 
-      photos: []
-    };
-  }
-
-
-componentDidMount()
-{
-  let uri = 'https://www.reddit.com/r/aww.json'
-fetch(uri)
-.then((data)=>{
-  return data.json();
-})
-.then((data)=>{
-  var imageUrls = [];
-  data.data.children.forEach(post=> 
-    {
-      if (post.data.thumbnail !== 'default'){
-   imageUrls.push(post.data.thumbnail)}
-  })
-  this.setState({photos : imageUrls});
-})
-}
-
 render()
 {
   return (
     <section className="App">
-      <div className="App-header">
-        <p>
-          Some cute images to cheer you up:
-        </p>
-        <PhotoContainer photos={this.state.photos}/>
-        </div>
+     <div className="container">
+       <h1 className="title"> Cheer up! Here's some cute stuff.</h1>
+       <SearchPhotos />
+     </div>    
     </section>
   );
 }
