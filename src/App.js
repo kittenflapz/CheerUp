757731b52b0aec033b25137a12c90c7a5c3fa65e
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-11-12 09:23:23 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-
-import './App.css';
+import "./App.css";
 import { Component } from "react";
 import SearchPhotos from "./SearchPhotos";
 
-
-class App extends Component
-{
-render()
-{
-  return (
-    <section className="App">
-     <div className="container">
-       <h1 className="title"> Cheer up. Here's some cute stuff.</h1>
-       <SearchPhotos />
-     </div>    
-    </section>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <section className="App">
+        <div className="container">
+          <h1 className="title"> Cheer up. Here's some cute stuff.</h1>
+          <SearchPhotos />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
--- src/SearchPhotos.js
+++ src/SearchPhotos.js
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
 
 export default function SearchPhotos() {
-    const [query, setQuery] = useState("");
-    const [pics, setPics] = useState([]);
-    const searchPhotos = async (e) => {
-        e.preventDefault();
-        let uri = 'https://www.reddit.com/r/aww/search.json?q=' + query + '&restrict_sr=true&limit=50';
-        fetch(uri)
-        .then((data)=>{
-            return data.json();
-          })
-          .then((data)=>{
-            var imageUrls = [];
-            data.data.children.forEach(post=> 
-              {
-                if (post.data.thumbnail !== 'default' && post.data.thumbnail !== 'self'){
-             imageUrls.push(post.data)}
-            });
-            setPics(imageUrls)
-        })
-        
-    }
-    
+  const [query, setQuery] = useState("");
+  const [pics, setPics] = useState([]);
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+    let uri =
+      "https://www.reddit.com/r/aww/search.json?q=" +
+      query +
+      "&restrict_sr=true&limit=50";
+    fetch(uri)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        var imageUrls = [];
+        data.data.children.forEach((post) => {
+          if (
+            post.data.thumbnail !== "default" &&
+            post.data.thumbnail !== "self"
+          ) {
+            imageUrls.push(post.data);
+          }
+        });
+        setPics(imageUrls);
+      });
+  };
+
   return (
     <>
-    <form className="form" onSubmit={searchPhotos}> 
-    <label className="label" htmlFor="query"> 
+      <form className="form" onSubmit={searchPhotos}>
+        <label className="label" htmlFor="query">
           {" "}
           🐱
         </label>
@@ -35,8 +40,7 @@ export default function SearchPhotos() {
           className="input"
           placeholder={`Try searching for your favourite cute thing!`}
           value={query}
-          onChange = {(e) => setQuery(e.target.value)}
-
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className="button">
           Search
@@ -44,18 +48,18 @@ export default function SearchPhotos() {
       </form>
 
       <div className="card-list">
-        {pics.map((pic)=> <div className="card" key={pic}> 
-        <a href={pic.url}>
-        <img
+        {pics.map((pic) => (
+          <div className="card" key={pic}>
+            <a href={pic.url}>
+              <img
                 className="card--image"
                 alt={pic.title}
                 src={pic.thumbnail}
-                
-                >
-         </img>
-         </a>
-         <text>{pic.title}</text>
-        </div>)}
+              ></img>
+            </a>
+            <text>{pic.title}</text>
+          </div>
+        ))}
       </div>
     </>
   );
