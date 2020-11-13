0f232e91bf5f7114a84b9f719a2a18bbf38981a1
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-11-13 10:06:13 -0500

    style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import SearchPhotos from "./SearchPhotos";
-import Footer from './footer'
+import Footer from "./footer";
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
         <div className="container">
           <h1 className="title">Cheer up. Here's some cute stuff.</h1>
           <SearchPhotos />
-          <Footer/>
+          <Footer />
         </div>
       </section>
     );
--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <p><a href={`https://github.com/kittenflapz/CheerUp`} rel="noopener noreferrer" target="_blank">See how this was made</a></p>
+    <p>
+      <a
+        href={`https://github.com/kittenflapz/CheerUp`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        See how this was made
+      </a>
+    </p>
   </div>
 );
 
